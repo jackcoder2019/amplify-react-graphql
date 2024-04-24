fd8ff37761024323a594723e62ac6da50a67c71b
Jielin Li <jielinli@Jielins-MacBook-Pro.local>
2024-04-23 22:58:26 -0700

    add GraphQL API

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "amplifyreactgraphql": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "amplifyreactgraphqlbe7ef9af": {
       "AppClientID": "string",
